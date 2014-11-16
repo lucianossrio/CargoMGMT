eef49bb4656ce035b433489c0357bbd5e128c0d1
Luciano Santos <lucianossrio@gmail.com>
2014-11-16 13:07:01 -0200

    Spring configs, banco e DAO

---
+++ src/main/java/com/luciano/cargomgmt/dao/CargoDAO.java
@@ -0,0 +1,53 @@
+package com.luciano.cargomgmt.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import com.luciano.cargomgmt.modelo.Cargo;
+
+@Repository
+public class CargoDAO {
+	
+	@PersistenceContext
+	private EntityManager manager;
+
+	public void salvar(Cargo cargo) {
+		if (cargo.getId() != null) {
+			this.manager.persist(cargo);
+		} else {
+			this.manager.merge(cargo);
+		}
+
+	}
+
+	public List<Cargo> allCargo() {
+		try {
+			return this.manager.createQuery("select c from Cargo c",
+					Cargo.class).getResultList();
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
+
+	public Cargo findCargoById(Integer id) {
+		try {
+			return this.manager
+					.createQuery(
+							"select c from Cargo c where c.id = :id",
+							Cargo.class)
+					.setParameter("id", id).getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
+
+	public void delete(Cargo cargo) {
+		this.manager.remove(cargo);
+		
+	}
+}
